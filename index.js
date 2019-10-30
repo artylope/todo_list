b69c5ab1d2eb6f4fb3a4cbfe8a09e9961896d06d
artylope <angyixin@gmail.com>
2019-10-30 12:04:32 +0800

    sites

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDMoXzV5-SCNyNK3lniSWPAhz8ob5avXys",
+    authDomain: "todo-list-2c04e.firebaseapp.com",
+    databaseURL: "https://todo-list-2c04e.firebaseio.com",
+    projectId: "todo-list-2c04e",
+    storageBucket: "todo-list-2c04e.appspot.com",
+    messagingSenderId: "1081091653179",
+    appId: "1:1081091653179:web:8b032e9cd2a250cf74261e",
+    measurementId: "G-QEGT1NQWWJ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  // firebase.analytics();
+
+var db = firebase.firestore();
+
+// Add new task to the task board
+function addNewItem(postit_text){
+   // Create a new post-it element for the task board
+   var new_postit = document.createElement("div");
+   new_postit.classList.add("new_postit");
+
+   // Create a post-it text element for the new post it
+   var postit_text_elem = document.createElement("p");
+   postit_text_elem.innerHTML = postit_text;
+
+   // Place the post-it text on the post-it
+   new_postit.appendChild(postit_text_elem);
+
+   // Place the post-it on the task board
+   document.getElementById("taskboard").appendChild(new_postit);
+};
+
 // This chunk of code will run when you press the submit button
 $("#postit").submit(function(e) {
     e.preventDefault();
@@ -8,22 +43,55 @@ $("#postit").submit(function(e) {
     var postit_text = postit_input.value;
     console.log("Text is:", postit_text)
 
-    // Add new task to the task board
-    function addNewItem(postit_text){
-//        // Create a new post-it element for the task board
-//        var new_postit = document.createElement("div");
-//        new_postit.classList.add("new_postit");
-//
-//        // Create a post-it text element for the new post it
-//        var postit_text_elem = document.createElement("p");
-//        postit_text_elem.innerHTML = postit_text;
-//
-//        // Place the post-it text on the post-it
-//        new_postit.appendChild(postit_text_elem);
-//
-//        // Place the post-it on the task board
-//        document.getElementById("taskboard").appendChild(new_postit);
-    };
     addNewItem(postit_text);
+
+    function saveItemToDatabase(postit_text){
+      doc = (db.collection("tasks")
+
+        //save tasks to database
+        .add({
+          mytask: postit_text
+        })
+
+        //then get the information sent to the database
+        .then(
+          function(docRef){
+            docRef.get();
+          }
+        )
+
+      );
+    };
+
+    saveItemToDatabase(postit_text);
+
+    function clearInput(){
+      document.getElementById("new-task").value ='';
+    }
+    clearInput();
 });
 
+
+// This chunk of code will run when the page is loaded
+$(document).ready(function(){
+
+   // Load data from database
+   function loadTasksFromDatabase(){
+
+       // Go to collection in database
+       db.collection("tasks")
+
+       // Get everything
+       .get()
+
+       // Then for each document in the collection
+       .then(function(querySnapshot){
+           querySnapshot.forEach(function(doc) {
+
+               // Display post-it on task board
+               addNewItem(doc.data()['mytask']);
+           });
+       });
+   };
+   loadTasksFromDatabase()
+});
